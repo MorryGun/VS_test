f4a787c3d15b40fba73fc47e8926c61fee7f180f
Kateryna Stepanenko <yasnopolyanka@gmail.com>
2019-12-13 19:17:22 +0200

    Basic algorythm added
    Delete function need to be updated

--- dummy/rate.py
+++ dummy/rate.py
@@ -11,3 +11,50 @@ def read_rate():
     rate_schema = RateSchema(many=True)
     data = rate_schema.dump(rate)
     return data
+
+
+def delete_rate(id):
+    delete_rate = Rate.query.filter(Rate.id == id).one_or_none()
+
+    if delete_rate is None:
+        abort(
+            404,
+            "Record not found for Id: {id}".format(id=id),
+        )
+
+    else:
+        db.session.delete(delete_rate)
+        db.session.commit()
+
+        return "Record is deleted"
+
+def check_current_rates(names):
+    current_rates = {}
+    for name in names:
+        current = 0
+        record = Rate.query.filter(Rate.name == name).one_or_none()
+        if record is None:
+            current = 1000
+        else:
+            current = record.rate
+        current_rates[name] = current
+    return current_rates
+
+def update_rate(update_rate_dict):
+    for record in update_rate_dict:
+        current_record = Rate.query.filter(Rate.name == record).one_or_none()
+        new_record = Rate(name=record, rate=update_rate_dict[record])
+        if current_record is None:
+            db.session.add(new_record)
+            db.session.commit()
+        else:
+            new_record.id = current_record.id
+
+            db.session.merge(new_record)
+            db.session.commit()
+
+def delete_all_rates():
+    rates = Rate.query.filter(Rate.rate!=1000).all()
+    for rate in rates: 
+        db.session.add(rate)
+    db.session.commit()
--- dummy/result.py
+++ dummy/result.py
@@ -1,6 +1,7 @@
 from flask import make_response, abort
 from dummy.config import db
 from dummy.models import Result, ResultSchema
+from dummy.rate import check_current_rates, update_rate
 
 
 def read_results():
@@ -13,25 +14,124 @@ def read_results():
     return data
 
 
-def add_result(results):
-    for result in results:
-        match_id = result.get("match_id")
-        name = result.get("name")
-        points = result.get("points")
-        date = result.get("date")
-    
-        schema = ResultSchema()
-        new_result = schema.load(result, session=db.session)
+def add_result(body):
+    matches = set()
 
-        db.session.add(new_result)
-        db.session.commit()
+    for result_array in body:
+        for result in result_array:
+            match_id = result.get("match_id")
+            name = result.get("name")
+            points = result.get("points")
+            date = result.get("date")
+
+            matches.add(match_id)
+    
+            schema = ResultSchema()
+            new_result = schema.load(result, session=db.session)
+
+            db.session.add(new_result)
+            db.session.commit()
+
+    calculate_rate(matches)
+        
     
     return "Result is added successfully"
 
 
-def replace_result():
-    return {'coming':'soon'}
+def replace_result(body):
+    matches = set()
+
+    result_id = body.get("result_id")
+    match_id = body.get("match_id")
+    name= body.get("name")
+    points = body.get("points")
+    date = body.get("date")
+    
+    update_result = Result.query.filter(
+        Result.result_id == result_id
+    ).one_or_none()
+
+    if update_result is None:
+        abort(
+            404,
+            "Result not found for Id: {result_id}".format(result_id=result_id),
+        )
+
+    else:
+        schema = ResultSchema()
+        update = schema.load(body, session=db.session)
+
+        matches.add(update_result.match_id)
+        calculate_rate(matches)
+
+        update.result_id = update_result.result_id
+
+        db.session.merge(update)
+        db.session.commit()
+
+        return "Record is updated"
 
 
-def delete_result():
-    return {'coming':'soon'}
+def delete_result(result_id):
+    matches = set()
+
+    delete_result = Result.query.filter(
+        Result.result_id == result_id
+    ).one_or_none()
+
+    if delete_result is None:
+        abort(
+            404,
+            "Result not found for Id: {result_id}".format(result_id=result_id),
+        )
+
+    else:
+        matches.add(delete_result.match_id)
+        calculate_rate(matches)
+        db.session.delete(delete_result)
+        db.session.commit()
+
+        return "Record is deleted"
+
+def calculate_rate(matches):
+    names = []
+    for match_id in matches:
+
+        # Create dict current_rates with names and current rates of the players by match_id
+        # Create list points with points of the player
+
+        selected_matches = Result.query.filter(Result.match_id == match_id).all()
+
+        points = []
+        players_points = {}
+
+        for selected_match in selected_matches:
+            names.append(selected_match.name)
+            points.append(selected_match.points)
+            players_points[selected_match.name] = selected_match.points
+
+        current_rates = check_current_rates(names)
+
+        # Calculate prize fond and sum of points for current match
+
+        fond = 0
+        for rate in current_rates:
+            fond = fond + current_rates[rate]*0.25
+
+
+        sum_of_game_points = 0
+        min_point = min(points) if min(points) < 0 else 0
+        for point in points:
+            sum_of_game_points = sum_of_game_points + point - min_point
+
+        # Calculate final rate after the match
+
+        if sum_of_game_points != 0:
+            for rate in current_rates:
+                prize = fond*(players_points[rate]-min_point)/sum_of_game_points
+                current_rates[rate] = round(0.75 * current_rates[rate] + prize)
+
+        # Update rates in rate table
+
+        update_rate(current_rates)
+
